6df5b5d65e4843790622fa331c51662948a444e0
unknown <leonardocabrera91@gmail.com>
2024-06-12 19:11:23 -0300

    first release

---
+++ src/main/java/com/ejercicios/MainApplication.java
@@ -0,0 +1,17 @@
+package com.ejercicios;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class MainApplication {
+
+    public static void main( String[] args ){
+
+		
+	}
+}
